d679b5bcf9de55bcabecc2d13dd6f7755101bb5e
Yonas Jongkind <yonas.jongkind@gmail.com>
2018-01-30 10:51:41 -0600

    Pushing these changes over to the mac installer.

--- admin/win/docker/yxj_run.sh
+++ admin/win/docker/yxj_run.sh
@@ -3,3 +3,5 @@
 sudo docker run -v "/home/yonasj/client:/home/yonasj/client" owncloud-client-win32:2.3.5 bash -x /home/yonasj/client/admin/win/docker/build.sh client/ $(id -u)
 
 cp build-win32/VaultDrop-2.5.0.0git-setup.exe /media/sf_VaultDrop/
+
+
