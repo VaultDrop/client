ce10da398e3a0c77c1a25ad2ee6326e1c8535218
Yonas Jongkind <yonas.jongkind@gmail.com>
2018-02-01 10:31:55 -0600

    Add file used to run CMAKE scripts:

--- client-build/YXJ_CMAKE.sh
+++ client-build/YXJ_CMAKE.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+
 cmake -DCMAKE_INSTALL_PREFIX=../install/ -D_OPENSSL_LIBDIR=/usr/local/opt/openssl/lib/ -D_OPENSSL_INCLUDEDIR=/usr/local/opt/openssl/include/ -DOPENSSL_INCLUDE_DIR=/usr/local/opt/openssl/include/ -DCMAKE_PREFIX_PATH=/usr/local/Cellar/qt/5.10.0_1/lib -DNO_SHIBBOLETH=1 ..
 make
 make install
-admin/osx/create_mac.sh ../install .
+admin/osx/create_mac.sh ../install . "3rd Party Mac Developer Installer: Vault Drop, LLC (9968GEYSTF)"
+
