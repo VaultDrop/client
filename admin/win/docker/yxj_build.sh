3165f38395e8f292835be3de19acbddc3b90a10c
Yonas Jongkind <yonas.jongkind@gmail.com>
2018-02-06 11:12:28 -0600

    Updated script to code sign window executable.

--- admin/win/docker/yxj_build.sh
+++ admin/win/docker/yxj_build.sh
@@ -1,13 +1,23 @@
 #!/bin/bash -x
 
 sudo docker run -v "/home/yonasj/client:/home/yonasj/client" owncloud-client-win32:2.3.5 bash -x /home/yonasj/client/admin/win/docker/build.sh client/ $(id -u)
-
-sudo cp ../../../build-win32/VaultDrop-2.*-setup.exe /media/sf_VaultDrop/client/yxj_autoupdate_versions
-
 VDVER=$(grep MIRALL_VERSION_FULL ../../../build-win32/version.h | sed -e 's/#define MIRALL_VERSION_FULL //' | sed -e 's/[" ]//g')
 
-sudo sed -e "s/VALUT_DROP_VERSION/${VDVER}/g" /media/sf_VaultDrop/client/yxj_autoupdate_versions/download.html.in -e 'w /media/sf_VaultDrop/client/yxj_autoupdate_versions/download.html'
-sudo sed -e "s/VALUT_DROP_VERSION/${VDVER}/g" /media/sf_VaultDrop/client/yxj_autoupdate_versions/macos.xml.in -e 'w /media/sf_VaultDrop/client/yxj_autoupdate_versions/macos.xml'
-sudo sed -e "s/VALUT_DROP_VERSION/${VDVER}/g" /media/sf_VaultDrop/client/yxj_autoupdate_versions/win32.xml.in -e 'w /media/sf_VaultDrop/client/yxj_autoupdate_versions/win32.xml'
 
+SIGNKEY=`cat code_sign_key`
+
+
+osslsigncode -pkcs12 ../../../yxj_vaultdrop_certificate/VaultDropCodeSigningKey.pfx -h sha256 \
+          -pass $SIGNKEY \
+          -n "VaultDrop Client" \
+          -i "http://www.VaultDrop.com" \
+          -ts "http://timestamp.comodoca.com" \
+          -in ../../../build-win32/VaultDrop-${VDVER}-git-setup.exe \
+          -out ../../../build-win32/VaultDrop-${VDVER}-cs-setup.exe
+
+sed -e "s/VALUT_DROP_VERSION/${VDVER}/g" '../../../yxj_autoupdate_versions/download.html.in' > '../../../yxj_autoupdate_versions/download.html'
+sed -e "s/VALUT_DROP_VERSION/${VDVER}/g" '../../../yxj_autoupdate_versions/macos.xml.in' > '../../../yxj_autoupdate_versions/macos.xml'
+sed -e "s/VALUT_DROP_VERSION/${VDVER}/g" '../../../yxj_autoupdate_versions/win32.xml.in' > '../../../yxj_autoupdate_versions/win32.xml'
+
+sudo cp ../../../build-win32/VaultDrop-2.*-setup.exe ../../../yxj_autoupdate_versions/*.xml ../../../yxj_autoupdate_versions/*.html /media/sf_VaultDrop/client/yxj_autoupdate_versions/
 
--- admin/win/docker/yxj_mkdockerimage.sh
+++ admin/win/docker/yxj_mkdockerimage.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-docker build . -t owncloud-client-win32:
+
+sudo docker build . -t owncloud-client-win32:2.3.5
 
