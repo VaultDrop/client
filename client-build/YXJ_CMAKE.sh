41cfdd26ae807b989b3542e035b9a1670c7c85c8
Yonas Jongkind <yonas.jongkind@gmail.com>
2018-02-16 15:25:31 -0600

    Sign the app as well as the installer now. Makes the keychain acceess request cleaner.

--- client-build/YXJ_CMAKE.sh
+++ client-build/YXJ_CMAKE.sh
@@ -3,7 +3,8 @@
 rm CMakeCache.txt
 cmake -DCMAKE_INSTALL_PREFIX=../install -D_OPENSSL_LIBDIR=/usr/local/opt/openssl/lib/ -D_OPENSSL_INCLUDEDIR=/usr/local/opt/openssl/include/ -DOPENSSL_INCLUDE_DIR=/usr/local/opt/openssl/include/ -DCMAKE_PREFIX_PATH=/usr/local/Cellar/qt/5.10.0_1/lib -DNO_SHIBBOLETH=1 ..
 make
-# Sign installer here???
+# Sign the app before we make the installer.
+../admin/osx/sign_app.sh "bin/VaultDrop.app" "Developer ID Application: Vault Drop, LLC (9968GEYSTF)"
 make install
 admin/osx/create_mac.sh ../install . "Developer ID Installer: Vault Drop, LLC (9968GEYSTF)"
 
@@ -11,7 +12,7 @@ VDVER=$(grep MIRALL_VERSION_FULL version.h | sed -e 's/#define MIRALL_VERSION_FU
 sed -e "s/VALUT_DROP_VERSION/${VDVER}/g" '../yxj_autoupdate_versions/macos.xml.in' > '../yxj_autoupdate_versions/macos.xml'
 sed -e "s/VALUT_DROP_VERSION/${VDVER}/g" '../yxj_autoupdate_versions/macos-sparkle.xml.in' > '../yxj_autoupdate_versions/macos-sparkle.xml'
 cp -v ../install/VaultDrop-${VDVER}-git.pkg ../install/VaultDrop-${VDVER}-git.pkg.tbz ../yxj_autoupdate_versions/
-cp -v ../install/VaultDrop-${VDVER}-git.pkg ../yxj_autoupdate_versions/VaultDrop-${VDVER}.pkg
+cp -v ../install/VaultDrop-${VDVER}-git.pkg ../yxj_autoupdate_versions/VaultDrop.pkg
 
 
 
